8423b6c01b5ba4e0dfc90ea21e2fcb790177e19a
vmarkov <vmarkov@hiplabs.dev>
2025-06-08 14:03:32 +0500

    refactor

--- hosts/mac-cero/default.nix
+++ hosts/mac-cero/default.nix
@@ -9,58 +9,6 @@
     ../../systems/nixos
   ];
 
- nixpkgs.config.allowUnfree = true;
-
-  security = {
-    sudo.wheelNeedsPassword = false;
-  };
-
-  services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-
-    settings = {
-      experimental-features = "nix-command flakes";
-      auto-optimise-store = false;
-      warn-dirty = false;
-      trusted-users = [ "withoutboat" ];
-    };
-  };
-
-  # Set your time zone.
-  time.timeZone = "Asia/Yekaterinburg";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
-  };
-
-  system.stateVersion = "24.11";
-
-
  # home-manager = {
  #   useUserPackages = true;
  #   users.mark = import ./home.nix;
