31c92c67ccd2e35734d703792080209859418153
vmarkov <vmarkov@hiplabs.dev>
2025-06-11 00:52:30 +0500

    feat: sops

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,12 +1,12 @@
 { pkgs, config, ... }:
 { 
-  sops.secrets.primary.neededForUsers = true;
-  users.mutableUsers = false;
+#  sops.secrets.primary.neededForUsers = true;
+#  users.mutableUsers = false;
   
   users.users = {
     withoutboat = {
      shell = pkgs.zsh;
-     hashedPasswordFile = config.sops.secrets.primary.path;
+#     hashedPasswordFile = config.sops.secrets.primary.path;
      isNormalUser = true;
      extraGroups = [
        "wheel"
