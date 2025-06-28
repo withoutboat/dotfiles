256884b5c805f77fbf017782372178de78fe64f9
Vladimr Markov <withoutboat@gmail.com>
2025-06-28 04:34:08 -0500

    feat: wg

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,81 +3,50 @@
   host,
   config,
   ...
-}: let
-  wgInterface = "amneziawg";
-  secretName = "wg0.conf";
-  secretPath = config.sops.secrets."${secretName}".path;
-in {
-  sops = {
-    secrets = {
-      ${secretName} = {
-        sopsFile = ../../secrets/wg0.conf;
-        mode = "0400";
-        owner = "root";
-        format = "binary";
-      };
-    };
+}: {
+  sops.secrets.amneziawg = {
+    sopsFile = ../../secrets/wg0.conf;
+    mode = "0400";
+    owner = "root";
+    format = "binary";
   };
 
   services.resolved.enable = true;
 
-  systemd.services.amneziawg-config = {
-    description = "Install AmneziaWG WireGuard configuration (with max debugging)";
+  boot.extraModulePackages = with pkgs; [amneziawg];
+  boot.kernelModules = ["amneziawg"];
+
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+  ];
+
+  systemd.services.amneziawg-setup = {
+    description = "Setup AmneziaWG Kernel Module with config";
     wantedBy = ["multi-user.target"];
-    before = ["wg-quick-${wgInterface}.service"];
+    after = ["network-online.target"];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
+      ExecStart = ''
+        # Adjust the config path as necessary
+        cat ${config.sops.secrets.amneziawg.path} > /dev/amneziawg
+      '';
     };
-    script = ''
-      set -x
-
-      # Check secret path
-      if [ -e "${secretPath}" ]; then
-        echo "[$(date)] Secret exists at: ${secretPath}"
-      else
-        echo "[$(date)] ERROR: Secret missing at: ${secretPath}"
-        ls -l $(dirname "${secretPath}")
-        exit 1
-      fi
-
-      # Ensure /etc/wireguard exists
-      mkdir -pv /etc/wireguard
-      # Install config
-      install -v -m 600 -o root -g root "${secretPath}" /etc/wireguard/${wgInterface}.conf
-
-    '';
   };
 
   networking = {
     hostName = "${host}";
-    networkmanager = {
+    networkmanager.enable = true;
+    networkmanager.dns = "systemd-resolved";
+    nat = {
       enable = true;
-      dns = "systemd-resolved";
+      externalInterface = "";
+      internalInterfaces = ["amneziawg0"];
     };
-    #   timeServers = options.networking.timeServers.default ++ ["pool.ntp.org"];
     firewall = {
       enable = true;
-      allowedTCPPorts = [
-        22
-        80
-        443
-        59010
-        59011
-        8080
-      ];
-      allowedUDPPorts = [
-        59010
-        59011
-        51820
-      ];
+      allowedTCPPorts = [22 80 443 59010 59011 8080];
+      allowedUDPPorts = [59010 59011 51820];
     };
   };
-
-  networking.wg-quick.interfaces.${wgInterface} = {
-    configFile = "/etc/wireguard/${wgInterface}.conf";
-    autostart = true;
-  };
-
-  environment.systemPackages = with pkgs; [networkmanagerapplet wireguard-tools];
 }
