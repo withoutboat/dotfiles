8094efac54a34152baf429fa3a559a5253b8d825
Vladimr Markov <withoutboat@gmail.com>
2025-07-02 19:06:20 -0500

    feat: core as flake

--- hosts/mac-cero/flake.nix
+++ hosts/mac-cero/flake.nix
@@ -4,11 +4,24 @@
   outputs = _: {
     nixosModules = {
       default = {
+        lib,
+        config,
+        ...
+      }: {
         imports = [
           ./hardware.nix
           ./host-packages.nix
         ];
-        config.environment.variables = import ./variables.nix;
+
+        options.host.variables = lib.mkOption {
+          type = lib.types.attrs;
+          default = {};
+          description = "Host-specific arbitrary variables available everywhere";
+        };
+
+        config = {
+          host.variables = import ./variables.nix;
+        };
       };
     };
   };
