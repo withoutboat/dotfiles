195b39794ea6ab92a5a93deebc9be3a028988edd
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 00:37:28 -0500

    feat: stylix

--- modules/core/stylix/default.nix
+++ modules/core/stylix/default.nix
@@ -1,20 +1,50 @@
 {
   pkgs,
   host,
+config,
   ...
-}: {
+}:  {
   # Styling Options
   stylix = {
   enable = true;
-  base16Scheme = ./frappe.yaml;
+
+ # theme = "catppuccin-frappe";
+ # base16Scheme = {
+ #   src = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+ # };
+
+  base16Scheme = {
+    base00 = "303446"; # base
+    base01 = "292c3c"; # mantle
+    base02 = "414559"; # surface0
+    base03 = "51576d"; # surface1
+    base04 = "626880"; # surface2
+    base05 = "c6d0f5"; # text
+    base06 = "f2d5cf"; # rosewater
+    base07 = "babbf1"; # lavender
+    base08 = "e78284"; # red
+    base09 = "ef9f76"; # peach
+    base0A = "e5c890"; # yellow
+    base0B = "a6d189"; # green
+    base0C = "81c8be"; # teal
+    base0D = "8caaee"; # blue
+    base0E = "ca9ee6"; # mauve
+    base0F = "eebebe"; # flamingokj:w
+
+    };
+
   image = host.stylixImage;
+
   polarity = "dark";
+
   opacity.terminal = 1.0;
+
   cursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 24;
   };
+
   fonts = {
     monospace = {
       package = pkgs.nerd-fonts.blex-mono;
