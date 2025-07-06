379d5b22cef45e55916ef121030e37971146eefc
Vladimr Markov <withoutboat@gmail.com>
2025-07-05 22:07:22 -0500

    feat: stylix

--- modules/core/stylix/default.nix
+++ modules/core/stylix/default.nix
@@ -5,16 +5,16 @@
 }: {
   # Styling Options
   stylix = {
-    enable = true;
-    base16Scheme = ./frappe.yaml;
-    image = host.stylixImage;
-    polarity = "dark";
-    opacity.terminal = 1.0;
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
-    };
+  enable = true;
+  base16Scheme = ./frappe.yaml;
+  image = host.stylixImage;
+  polarity = "dark";
+  opacity.terminal = 1.0;
+  cursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
   fonts = {
     monospace = {
       package = pkgs.nerd-fonts.blex-mono;
@@ -31,13 +31,13 @@
     emoji = {
       package = pkgs.noto-fonts-emoji;
       name = "Noto Color Emoji";
-    };   
-      sizes = {
-        applications = 12;
-        terminal = 15;
-        desktop = 11;
-        popups = 12;
-      };
+    };
+    sizes = {
+      applications = 12;
+      terminal = 15;
+      desktop = 11;
+      popups = 12;
     };
   };
+};
 }
